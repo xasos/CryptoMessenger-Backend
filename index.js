d1ac355c7242b09ddced223b05d22c39a39dc70e
Niraj Pant <nirajpant7@gmail.com>
2015-02-22 01:42:05 -0600

    update package deps, fix license, add error handling

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var mongoose = require('mongoose');
 var config = require('./config');
 var app = express();
 
+// Set port to listen on and define public route
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+// Connect to database from config file
 mongoose.connect(config.mongoURL, function(error) {
 	if (error) {
 		console.log(error);
@@ -19,20 +21,25 @@ var UserSchema = new Schema({
     publicKey: String
 });
 
-// Mongoose Model definition
+// Mongoose Model Definition
 var Users = mongoose.model('users', UserSchema);
 
+// Define Routes
 app.get('/', function(request, response) {
 	response.send('Welcome to CryptoMessenger!');
 });
 
+// Route to create user
 app.post('/createuser', function(req, res) {
+	// Check to see if user exists
 	Users.findOne({phoneNumber: req.body.phoneNumber}, function(err, user) {
-		if (err) return handleError(err);
+		if (err) return handleError(err); // Handle error
+		// Handle if user exists
 		if (user) {
 			res.writeHead(403, "Forbidden", {'Content-Type': 'text/html'});
 			res.json({"error": "User already exists"});
 		} else {
+			// Create user from route parameters and add to database
 			var newUser = new Users({ phoneNumber: req.body.phoneNumber, publicKey: req.body.publicKey });
 			newUser.save(function() {
 				if (err) return handleError(err);
@@ -42,17 +49,25 @@ app.post('/createuser', function(req, res) {
 	});
 });
 
+// Route to get user data 
 app.get('/getuser/:phoneNumber', function(req, res) {
+	// Find user in database using phone number as a key
 	Users.findOne({phoneNumber: req.params.phoneNumber}, function(err, user) {
-		if (err) return handleError(err);
+		if (err) return handleError(err); // Handle Error
+		// If user exists return data
 		if (user) {
 			res.json({ phoneNumber: user.phoneNumber, publicKey: user.publicKey });
+		} else {
+			res.writeHead(404, "User Doesn't Exist", {'Content-Type': 'text/html'});
 		}
 	});
 });
 
-app.post('/deleteuser', function(req, res) {
-  response.send('Hello World!');
+app.delete('/deleteuser/:phoneNumber', function(req, res) {
+	Users.findOneAndRemove({phoneNumber: req.body.phoneNumber}, function(err, user) {
+		if (err) return handleError(err);
+		res.writeHead(200, "User Successfully Deleted", {'Content-Type': 'text/html'});
+	});
 });
 
 app.listen(app.get('port'), function() {
