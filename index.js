b082605e56074e83f3d1cdab013d7c3475d3eb2f
Niraj Pant <nirajpant7@gmail.com>
2015-02-22 13:06:48 -0600

    fix status code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config');
+var bodyParser = require('body-parser');
+// var User = require('./User');
 var app = express();
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
 
 // Set port to listen on and define public route
 app.set('port', (process.env.PORT || 5000));
@@ -17,8 +22,8 @@ mongoose.connect(config.mongoURL, function(error) {
 // Mongoose Schema Definition
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
-    phoneNumber: String,
-    publicKey: String
+    phoneNumber: { type:String, required:true, min:1 },
+    publicKey: { type:String, required:true, min:1 }
 });
 
 // Mongoose Model Definition
@@ -32,18 +37,17 @@ app.get('/', function(request, response) {
 // Route to create user
 app.post('/createuser', function(req, res) {
 	// Check to see if user exists
-	Users.findOne({phoneNumber: req.body.phoneNumber}, function(err, user) {
+	users.find({ phoneNumber: req.body.phoneNumber }, function(err, user) {
 		if (err) return handleError(err); // Handle error
 		// Handle if user exists
 		if (user) {
-			res.writeHead(403, "Forbidden", {'Content-Type': 'text/html'});
-			res.json({"error": "User already exists"});
+			res.send(403).json({error: "User already exists"});
 		} else {
 			// Create user from route parameters and add to database
 			var newUser = new Users({ phoneNumber: req.body.phoneNumber, publicKey: req.body.publicKey });
 			newUser.save(function() {
 				if (err) return handleError(err);
-				res.writeHead(200, "User Successfully Added", {'Content-Type': 'text/html'});
+				res.status(200).json({ message: "User Successfully Added" });
 			});
 		}
 	});
@@ -51,25 +55,32 @@ app.post('/createuser', function(req, res) {
 
 // Route to get user data 
 app.get('/getuser/:phoneNumber', function(req, res) {
-	// Find user in database using phone number as a key
-	Users.findOne({phoneNumber: req.params.phoneNumber}, function(err, user) {
+	// Find user in database using phone number as key
+	Users.findOne({ phoneNumber: req.params.phoneNumber}, function(err, user) {
+		console.log("works");
+		console.log(user);
 		if (err) return handleError(err); // Handle Error
 		// If user exists return data
 		if (user) {
+			console.log("works2");
 			res.json({ phoneNumber: user.phoneNumber, publicKey: user.publicKey });
 		} else {
-			res.writeHead(404, "User Doesn't Exist", {'Content-Type': 'text/html'});
+			console.log("umm");
+			res.status(404).json({ message: "User Doesn't Exist" });			
 		}
 	});
 });
 
+// Route to delete user
 app.delete('/deleteuser/:phoneNumber', function(req, res) {
-	Users.findOneAndRemove({phoneNumber: req.body.phoneNumber}, function(err, user) {
-		if (err) return handleError(err);
-		res.writeHead(200, "User Successfully Deleted", {'Content-Type': 'text/html'});
+	// Find user and delete in database using phone number as key
+	Users.findOneAndRemove({ phoneNumber: req.params.phoneNumber }, function(err, user) {
+		if (err) return handleError(err); // Handle Error
+		res.status(200).json({ message:  "User Successfully Deleted" });
 	});
 });
 
+// Open app to listen on specified port
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
